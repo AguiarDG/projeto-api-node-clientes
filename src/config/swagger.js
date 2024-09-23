06afc051ac497754bd2dcbf814c108884d3d18b7
AguiarDG <aguiardg90@gmail.com>
2024-09-23 08:49:25 -0300

    Adicionando variavel de ambiente BASE_URL

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -2,8 +2,19 @@ import swaggerUi from "swagger-ui-express";
 import swaggerOptions from "./swagger.json" with { type: "json" };
 
 const setupSwaggerDocs = app => {
+  const replacePlaceholders = (swaggerJson, envVars) => {
+    let jsonStr = JSON.stringify(swaggerJson);
+    for (const [key, value] of Object.entries(envVars)) {
+      jsonStr = jsonStr.replace(new RegExp(`\\$\\{${key}\\}`, "g"), value);
+    }
+    return JSON.parse(jsonStr);
+  };
+
+  const swaggerWithEnv = replacePlaceholders(swaggerOptions, {
+    BASE_URL: process.env.BASE_URL,
+  });
   // Define a rota /api-docs para acessar o Swagger UI
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOptions));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerWithEnv));
 };
 
 export default setupSwaggerDocs;
